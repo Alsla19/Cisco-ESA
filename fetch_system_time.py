e3b8f52d83bc1584a17364ad247bc35e18ee26a3
Ahsan Abbas <58534995+Alsla19@users.noreply.github.com>
2024-05-01 18:48:44 +0530

    Fetch System Time

---
+++ fetch_system_time.py
@@ -0,0 +1,18 @@
+import requests
+import json
+import pprint
+url = "http://10.106.36.175:6080/esa/api/v2.0/config/system_time?device_type=esa"
+
+payload={}
+headers = {
+  'Authorization': 'Basic YWRtaW46S2xxxxxxxxxxxxxxxxxxxxx',
+  'Cookie': 'thenetworkviking'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload, verify="False")
+
+json_string = json.loads(response.content)
+
+pprint.pprint(json_string)
+
+
